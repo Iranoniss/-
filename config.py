d25901b1d3b5f38c3d8897c13df80739e6fffd49
Iranoniss <123898302+Iranoniss@users.noreply.github.com>
2023-04-19 13:38:22 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "6237982538:AAExl9BFUlb8GRXogRB8y2F8oZpabK7utmg"
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'гривна': 'UAH',
+    'фунт': 'GBR',
+    'франк': 'CHF',
+    'йена': 'JPY',
+    'юань': 'CNY',
+    'биткоин': 'BTC',
+}
